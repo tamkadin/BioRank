816027548e222ac8b287d8c2a098cf0a020f30e3
tamkadin <tamkadinner@gmail.com>
2025-05-29 14:50:45 +0700

    update: code clean

--- improved_pagerank/core/page_rank_ori.py
+++ improved_pagerank/core/page_rank_ori.py
@@ -4,34 +4,30 @@ CONV_THRESHOLD = 0.000001
 
 class PageRankOri:
     def __init__(self,
-                 G,                        # Đồ thị
-                 restart_prob=0.85):       # Hệ số giảm chấn (damping factor)
+                 G,
+                 restart_prob=0.85):
 
         self.restart_prob = restart_prob
-        self.G = self.__normalize_graph__(G)  # Chuẩn hóa đồ thị
+        self.G = self.__normalize_graph__(G)
     
     def __compute_next_page_rank__(self, p_t):
         next_p_t = {}
         damping_factor = self.restart_prob
-        n = len(p_t)  # Số lượng nút trong đồ thị
+        n = len(p_t)
 
         for i in p_t.keys():
-            # Giá trị khởi tạo (1 - d)/n
             rank_sum = (1 - damping_factor) / n
             
-            for j in self.G.predecessors(i):  # Các nút có cạnh hướng tới i
-                out_degree_j = self.G.out_degree(j)  # Chỉ lấy bậc đi ra
+            for j in self.G.predecessors(i):
+                out_degree_j = self.G.out_degree(j)
                 if out_degree_j > 0:
                     rank_sum += damping_factor * (p_t[j] / out_degree_j)
 
-            next_p_t[i] = rank_sum  # Lưu giá trị PageRank mới của nút i
+            next_p_t[i] = rank_sum
 
         return next_p_t
 
     def __generate_ranked_list__(self, page_rank_vector):
-        """
-        Sắp xếp các nút dựa trên điểm PageRank.
-        """
         generate_probabilities = []
         for k, v in page_rank_vector.items():
             generate_probabilities.append([k, v])
@@ -39,15 +35,9 @@ class PageRankOri:
         return sorted_list
 
     def __norm_l1__(self, p_t_1, p_t):
-        """
-        Tính độ khác biệt giữa hai vector PageRank liên tiếp.
-        """
         return sum(abs(p_t_1[gene] - p_t[gene]) for gene in p_t_1)
 
     def __normalize_graph__(self, G):
-        """
-        Chuẩn hóa trọng số của đồ thị để tổng trọng số từ một nút bất kỳ bằng 1.
-        """
         G_normalized = nx.DiGraph()
 
         for node_1 in G:
@@ -62,9 +52,6 @@ class PageRankOri:
 
         return G_normalized
     def run(self):
-        """
-        Thực thi thuật toán PageRank cho đến khi hội tụ.
-        """
         n = len(self.G.nodes())
         p_v = {node: 1 / n for node in self.G.nodes()} 
         diff_norm = 1
@@ -72,8 +59,4 @@ class PageRankOri:
             p_t_1 = self.__compute_next_page_rank__(p_v)
             diff_norm = self.__norm_l1__(p_t_1, p_v)
             p_v = p_t_1
-        out_degrees = [self.G.out_degree(node) for node in self.G.nodes()]
-
-        print(f"Bậc đi ra thấp nhất: {min(out_degrees)}")
-        print(f"Bậc đi ra cao nhất: {max(out_degrees)}")
         return self.__generate_ranked_list__(p_v)
